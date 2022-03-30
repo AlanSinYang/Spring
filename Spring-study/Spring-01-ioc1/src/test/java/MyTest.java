dfb7b9bc4964c3c0822024682508f1e41decc2d3
MrYang02 <102336505+MrYang02@users.noreply.github.com>
2022-03-30 23:04:33 +0800

    Add files via upload

---
+++ Spring-study/Spring-01-ioc1/src/test/java/MyTest.java
@@ -0,0 +1,19 @@
+import com.kuang.dao.UserDaoImpl;
+import com.kuang.dao.UserDaoMysqlImpl;
+import com.kuang.dao.UserDaoOracleImpl;
+import com.kuang.service.UserService;
+import com.kuang.service.UserServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyTest {
+    public static void main(String[] args) {
+
+        //去获取ApplicationContext;拿到Spring的容器
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+        //容器在手里，需要什么 就直接get什么
+        UserServiceImpl userServiceImpl = (UserServiceImpl) context.getBean("UserServiceImpl");
+        userServiceImpl.getUser();
+    }
+}
