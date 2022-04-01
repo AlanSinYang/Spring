17bed91a12453057969668ff9d69e058f305f4a6
MrYang02 <102336505+MrYang02@users.noreply.github.com>
2022-04-01 10:07:35 +0800

    Delete Spring-study directory

--- Spring-study/Spring-01-ioc1/src/main/java/com/kuang/dao/UserDaoMysqlImpl.java
+++
@@ -1,7 +0,0 @@
-package com.kuang.dao;
-
-public class UserDaoMysqlImpl implements UserDao{
-    public void getUser(){
-        System.out.println("Hello");
-    }
-}
--- Spring-study/Spring-01-ioc1/src/main/java/com/kuang/dao/UserDaoOracleImpl.java
+++
@@ -1,8 +0,0 @@
-package com.kuang.dao;
-
-public class UserDaoOracleImpl implements UserDao{
-    public void getUser(){
-
-        System.out.println("Oracle获取ID数据");
-    }
-}
